941df504bd076ba77760995ac4ec00ba5ea9d362
ddb95 <debadatta1995@outlook.com>
2019-06-07 01:13:55 +0530

    Polar Coordinates left incomplete

---
+++ polarCoordinates.py
@@ -0,0 +1,8 @@
+import cmath
+import re
+
+n = input()
+n = n.split('+')
+# print(a)
+# print(abs(complex(int(a[0]), int(a[1]))))
+# print(cmath.phase(complex(int(a[0]), int(a[1]))))
--- symmetricDifference.py
+++ symmetricDifference.py
@@ -15,6 +15,3 @@ if __name__ == '__main__':
         print(main_list[i])
 
 
-
-# a, b = [set(input().split()) for _ in range(4)][1::2]
-# print(*sorted(a ^ b, key=int), sep='\n')
